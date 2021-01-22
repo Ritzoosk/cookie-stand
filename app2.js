212c1b92624402bfefd4faa1d99a6765a4c57f8d
Ritzoosk <knecht.ted@gmail.com>
2021-01-21 17:34:26 -0800

    css for sales page

--- app2.js
+++ app2.js
@@ -9,28 +9,21 @@ table.appendChild(tableHead);
 var tableBody = document.createElement('tbody');
 table.appendChild(tableBody);
 
-
 //Global Hours array + total//
 var standArray = [];
 var hrsOfOp = ['6am','7am','8am', '9am', '10am', '11am', '12am','1pm','2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'];
 
 //TABLE HEADER//
-
 var tableTopLine = document.createElement('th');
 tableTopLine.textContent = " ";
-
   tableHead.appendChild(tableTopLine);
-
 for (var i=0; i< hrsOfOp.length; i++){
   var tableTopLine = document.createElement('th');
   tableTopLine.textContent = hrsOfOp[i];
   tableHead.appendChild(tableTopLine);
 }
-// elementTarget.appendChild(tableHead);
-
 
 //City data storing CONSTRUCTOR//
-
 function CityStore(name, minCust, maxCust, avgCk,){
   this.name = name;
   this.minCust = minCust;
@@ -43,7 +36,6 @@ function CityStore(name, minCust, maxCust, avgCk,){
 }
 
 //Method for rndm # (and total cookies @[15]) generation store in "name.array"//
-
 CityStore.prototype.genRndCust = function(){
   var ckTtl = 0;
   for (var i = 0; i < hrsOfOp.length - 1 ; i++){ 
@@ -55,16 +47,13 @@ CityStore.prototype.genRndCust = function(){
 }
 
 //Print data to table//
-
 CityStore.prototype.printTable = function(){
   var row = document.createElement('tr')
   var nameCell = document.createElement('td')
   tableBody.appendChild(row);
   nameCell.textContent = this.name;
   row.append(nameCell);
-  
   for (var i=0; i< hrsOfOp.length; i++){
-    //console.log('made it in the loop', i)
     var dataCell = document.createElement('td');
     dataCell.textContent = this.array[i];
     row.appendChild(dataCell);
@@ -72,7 +61,6 @@ CityStore.prototype.printTable = function(){
 }
 
 //Construct OBJs, run random # cook-per-hour array with cookie ttl at end//
-
 var seattle = new CityStore('Seattle', 23, 65, 6.3,);
 var tokyo = new CityStore('Tokyo', 3, 24, 1.2,);
 var dubai = new CityStore('Dubai', 11, 38, 3.7,);
@@ -90,7 +78,6 @@ var tableFoot = document.createElement('tfoot');
     for(var j =0; j < standArray.length; j++){
       var grabNum = standArray[j].array;
       hourlyTotals += grabNum[i];
-      console.log(grabNum[i]);
     }
     var footCell = document.createElement('td');
     footCell.textContent = hourlyTotals;
@@ -99,7 +86,6 @@ var tableFoot = document.createElement('tfoot');
 
   //EVENT SUFF//
 var formSubmit = document.getElementById('newStand');
-
 formSubmit.addEventListener('submit', function (event) {
   event.preventDefault();
   var name = event.target.loc.value;
